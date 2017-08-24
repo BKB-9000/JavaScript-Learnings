ef2d60b6d450c038e450773b60895ea46cd7f337
David Baden-Powell <dbadenpowell@seek.com.au>
2017-08-24 10:56:52 +1000

    CodeAcademy files checkin

---
+++ console_log_stuff.js
@@ -0,0 +1,18 @@
+var myString1 = 'Dave';
+var myString2 = 'does';
+var myString3 = 'farts';
+var myNumber = '69';
+var myBoolean = false;
+var txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var sln = txt.length;
+
+// Do not edit the code under this line
+console.log("Name: " + myString1);
+console.log("Lucky Number: " + myNumber);
+console.log("Good joke? " + myBoolean);
+console.log(myString1 + " " + myString2 + " " + myString3);
+//console.getElementById("demo").innerHTML = Date();
+//window.alert(5 + 6);
+console.log(sln);
+console.warn(sln);
+//console.log(obj);
---
+++ timer.js
@@ -0,0 +1,10 @@
+//var str = JSON.stringify("blah blah blah");
+//console.log(str); // Logs output to dev tools console.
+//
+
+//console.log("This is %c My stylish message", "color: yellow; font-style: italic; background-color: blue;padding: 2px");
+
+
+console.time("answer time");
+console.log("Click to continue");
+console.timeEnd("answer time");
